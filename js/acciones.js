4ad497b3f2c6d258c2ad21397c11ac9dc91f1ea6
campos92 <vanessa3f.dsaupoo@gmail.com>
2016-05-12 13:31:19 -0500

    1subida
    octava aplicacion

---
+++ js/acciones.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+$(document) .ready(function (e){
+   document .addEventListener("deviceready",function (){
+	   $('#beep').tap(function(){
+		   navigator.notification.beep(1);//numero de veces que deseas que notifique 
+	   });//beep
+   $('#btnvibrar').on('tap', function(){
+	   navigator.notification.vibrate(5000);
+   });
+   });
+});
+	   
